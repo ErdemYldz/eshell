cd1b7d99d45643fad0d179f68ae035adbbbf1729
rdmyldz <n.erdemyildiz@gmail.com>
2021-01-02 23:41:27 +0300

    added > and >> redirections functionality

--- eshell.go
+++ eshell.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/exec"
 	"os/user"
+	"regexp"
 	"strings"
 )
 
@@ -69,6 +70,39 @@ func getPrompt() (string, error) {
 
 }
 
+func makeFile(filename string, flag int) (*os.File, error) {
+	f, err := os.OpenFile(filename, flag, 0666)
+	if err != nil {
+		return nil, err
+	}
+	return f, nil
+}
+
+var tokens = map[string]int{
+	">":  os.O_CREATE | os.O_WRONLY,
+	">>": os.O_APPEND | os.O_CREATE | os.O_WRONLY,
+}
+
+// checks the command if it has > or >>
+func checkCommand(cmd string) (string, int, bool) {
+	for k, v := range tokens {
+		// 3 because it must be bigger than the longest token
+		s := regexp.MustCompile(k).Split(cmd, 3)
+		if len(s) == 2 {
+			return k, v, true
+		}
+	}
+	return "", -1, false
+}
+func parseRedirections(cmd, token string) []string {
+	var ret []string
+	cmds := strings.Split(cmd, token)
+	for _, cmd := range cmds {
+		ret = append(ret, strings.Join(strings.Fields(cmd), " "))
+	}
+	return ret
+}
+
 func main() {
 	prompt, err := getPrompt()
 	if err != nil {
@@ -84,15 +118,47 @@ func main() {
 		if commands != nil {
 			var b bytes.Buffer
 			for _, cmd := range commands {
+				var filename string
+				var f *os.File
+				if token, flag, ok := checkCommand(cmd); ok {
+					reds := parseRedirections(cmd, token)
+					fmt.Printf("after parsed reds: %#v\n", reds)
+					cmd = reds[0]
+					filename = reds[1]
+					f, err = makeFile(filename, flag)
+					if err != nil {
+						fmt.Printf("error while creating %s:%s", filename, err)
+					}
+					if cmd == "" {
+						if token == ">" {
+							f.Truncate(0)
+							f.Close()
+							continue
+						}
+						if token == ">>" {
+							f.Close()
+							continue
+						}
+					}
+				}
+
 				ch, errc := runCmd(cmd, &b)
 				if err := <-errc; err != nil {
 					fmt.Println(err)
 				}
-				b = <-ch
+				if filename != "" {
+					b = <-ch
+					f.Write(b.Bytes())
+					b.Reset()
+					continue
+				} else {
+					b = <-ch
+				}
 			}
 			fmt.Println(b.String())
 		}
 
 		fmt.Print(prompt)
 	}
+
 }
